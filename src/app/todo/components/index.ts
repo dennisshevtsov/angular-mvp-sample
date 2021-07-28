a9bceb27d8b421d4f8d66d39b38ddb0b5c04a8f0
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-07-29 00:27:52 +0300

    simplify import

---
+++ src/app/todo/components/index.ts
@@ -0,0 +1,5 @@
+export * from './add-todo-list/add-todo-list.component';
+export * from './delete-todo-list/delete-todo-list.component';
+export * from './get-todo-list/get-todo-list.component';
+export * from './search-todo-lists/search-todo-lists.component';
+export * from './update-todo-list/update-todo-list.component';
