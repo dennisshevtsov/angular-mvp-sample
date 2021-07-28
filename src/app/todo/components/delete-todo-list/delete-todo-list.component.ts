8aa2f5e09eb61ff41183b46975852727f5a6432b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-07-29 00:24:44 +0300

    add todo list comps

---
+++ src/app/todo/components/delete-todo-list/delete-todo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-todo-list',
+  templateUrl: './delete-todo-list.component.html',
+  styleUrls: ['./delete-todo-list.component.scss']
+})
+export class DeleteTodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,10 +2,21 @@ import { NgModule, } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 
 import { TodoRoutingModule, } from './todo-routing.module';
+import { AddTodoListComponent, } from './components/add-todo-list/add-todo-list.component';
+import { UpdateTodoListComponent, } from './components/update-todo-list/update-todo-list.component';
+import { DeleteTodoListComponent, } from './components/delete-todo-list/delete-todo-list.component';
+import { SearchTodoListsComponent, } from './components/search-todo-lists/search-todo-lists.component';
+import { GetTodoListComponent, } from './components/get-todo-list/get-todo-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddTodoListComponent,
+    UpdateTodoListComponent,
+    DeleteTodoListComponent,
+    SearchTodoListsComponent,
+    GetTodoListComponent,
+  ],
   imports: [
     CommonModule,
     TodoRoutingModule,
