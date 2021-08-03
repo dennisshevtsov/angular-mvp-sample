1cf362938cd7d4dce3a3e74712a1f03078e7848f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-08-03 12:22:56 +0300

    add term

--- src/app/todo/components/search-todo-lists/search-todo-lists.presenter.ts
+++ src/app/todo/components/search-todo-lists/search-todo-lists.presenter.ts
@@ -1,6 +1,4 @@
-import {
-  SearchTodoListRecordResponseDto,
-  SearchTodoListRequestDto, } from '../../models';
+import { SearchTodoListRequestDto, } from '../../models';
 import { TodoListService, } from '../../services/todo-list.service';
 import { SearchTodoListsView, } from './search-todo-lists.view';
 
@@ -8,10 +6,12 @@ export class SearchTodoListsPresenter {
   public constructor(
     private readonly _view: SearchTodoListsView,
     private readonly _service: TodoListService,
-  ) {}
+  ) { }
 
   public search(): void {
-    const searchTodoListRequestDto = new SearchTodoListRequestDto();
+    const searchTodoListRequestDto = new SearchTodoListRequestDto(
+      this._view.query.term,
+    );
 
     this._view.datasource = this._service.searchTodoList(searchTodoListRequestDto);
   }
