833e3baefd046a8c46ac38bd0e26e881ee8f5d74
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-08-09 21:03:36 +0300

    format

--- src/app/todo/components/search-todo-list-tasks/search-todo-list-tasks.component.ts
+++ src/app/todo/components/search-todo-list-tasks/search-todo-list-tasks.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, } from '@angular/core';
+import { ActivatedRoute,    } from '@angular/router';
 
 import { TodoListTaskService,                  } from '../../services/todo-list-task.service';
 import { SearchTodoListTasksPresenter,         } from './search-todo-list-tasks.presenter';
 import { SearchTodoListTasksRecordResponseDto, } from '../../models';
 import { SearchTodoListTasksView,              } from './search-todo-list-tasks.view';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-browse-todo-list',
