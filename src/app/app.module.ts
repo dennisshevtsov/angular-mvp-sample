c322620f792e993cbf4f43eac0c57929fcc86228
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-08-12 00:21:13 +0300

    add todo list task module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule, } from '@angular/core';
+import { NgModule,      } from '@angular/core';
 import { BrowserModule, } from '@angular/platform-browser';
 
-import { AppRoutingModule, } from './app-routing.module';
-import { AppComponent, } from './app.component';
-import { TodoModule, } from './todo/todo.module';
+import { AppRoutingModule,   } from './app-routing.module';
+import { AppComponent,       } from './app.component';
+import { TodoListTaskModule, } from './todo-list-task';
+import { TodoModule,         } from './todo-list';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { TodoModule, } from './todo/todo.module';
   imports: [
     BrowserModule,
     TodoModule,
+    TodoListTaskModule,
     AppRoutingModule,
   ],
   providers: [],
---
+++ src/app/todo-list-task/todo-list-task.module.ts
@@ -0,0 +1,13 @@
+import { NgModule,     } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+
+import { TodoListTaskRoutingModule, } from './todo-list-task-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TodoListTaskRoutingModule,
+  ]
+})
+export class TodoListTaskModule { }
