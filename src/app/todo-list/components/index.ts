2d32c50f2d49ce387dc038f56894a380aa5e8c4c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-09-03 00:46:14 +0300

    refactor todo list imports

--- src/app/todo-list/components/index.ts
+++ src/app/todo-list/components/index.ts
@@ -1,4 +1,3 @@
 export * from './add-todo-list/add-todo-list.component';
-export * from '../../todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.component';
 export * from './search-todo-lists/search-todo-lists.component';
 export * from './update-todo-list/update-todo-list.component';
