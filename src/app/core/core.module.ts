72acff76ce24d990c55fc424f2dd6ad4915e13b2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-07 11:53:26 +0300

    add page component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule,     } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 
 import { ModalComponent, } from './components';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
   declarations: [
     ModalComponent,
+    PageComponent,
   ],
   imports: [
     CommonModule,
